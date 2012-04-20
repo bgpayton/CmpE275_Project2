7cdeb39c5bac635437375627eba7ba2fa156d08b
Peter Vera <petercvera@gmail.com>
2012-04-19 17:12:30 -0700

    initial crack at hearts service

---
+++ hearts.rb
@@ -0,0 +1,34 @@
+require 'sinatra'
+require 'mongo'
+
+get '/hearts/charities/' do
+  #check neighbors max hops = 
+  #return a list of charities
+  
+end
+
+get '/hearts/charities/:hops' do
+  #return a list of charities
+end
+
+post '/hearts/charities' do
+  #add a charity
+end
+
+delete '/hearts/charities' do
+  #remove a charity
+end
+
+get '/hearts/charities/:name' do
+  #return info about :name charity
+  params[:name]
+end
+
+get '/hearts/charities/:name/*' do
+  
+end
+
+
+
+
+
